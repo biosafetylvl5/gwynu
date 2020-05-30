6611d694fcd746e5b84c8a67a6221e5fa43c5196
BSL5 <biosafetylevel5@gmail.com>
2020-05-30 13:57:32 -0700

    Added EV spectrum, new build, test ftp push

---
+++ build/understanding/things-I-use.d
@@ -0,0 +1,5 @@
+<p>#Things I Use</p>
+<h2 id="software">Software</h2>
+<h3 id="data-processing">Data processing</h3>
+<p><a href="https://github.com/tabulapdf/tabula">tablua</a> - cleanly extracts data from PDF tables</p>
+<p><a href="https://apps.automeris.io/wpd/">WebPlotDigitizer</a> - for converting images of graphs to raw data</p>
